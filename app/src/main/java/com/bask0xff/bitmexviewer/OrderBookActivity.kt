32036be6978e5521eb72719d3a9aa734853b3491
sbaskov <sbaskov@callpanda.com>
2023-09-14 20:03:33 +0400

    show ticker name

--- app/src/main/java/com/bask0xff/bitmexviewer/OrderBookActivity.kt
+++ app/src/main/java/com/bask0xff/bitmexviewer/OrderBookActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -28,6 +29,7 @@ class OrderBookActivity : AppCompatActivity() {
 
         val symbol = intent.getStringExtra("SYMBOL")
         if (symbol != null) {
+            findViewById<TextView>(R.id.tickerName).text = symbol
             viewModel.initializeWebSocket(symbol)
         }
     }
